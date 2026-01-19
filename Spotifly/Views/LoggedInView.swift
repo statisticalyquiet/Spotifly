e1da3d57693f5772bdee2f37a9e4772213752106
Ralph von der Heyden <ralph@rvdh.de>
2026-01-19 17:42:18 +0100

    Queue artists

--- Spotifly/Views/LoggedInView.swift
+++ Spotifly/Views/LoggedInView.swift
@@ -319,6 +319,17 @@ struct LoggedInView: View {
 
         return Menu {
             Button {
+                Task {
+                    let token = await session.validAccessToken()
+                    await playbackViewModel.addToQueue(uri: artist.uri, accessToken: token)
+                }
+            } label: {
+                Label("Play Next", systemImage: "text.line.first.and.arrowtriangle.forward")
+            }
+
+            Divider()
+
+            Button {
                 if let externalUrl = artist.externalUrl {
                     let pasteboard = NSPasteboard.general
                     pasteboard.clearContents()
